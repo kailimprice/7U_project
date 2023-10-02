df87464dc4f4e63e9d6163c7a9507830ac504f59
kailimprice <kailimprice@gmail.com>
2023-10-01 22:08:45 -0400

    altered api call code to accommadate Slack API call response body

--- src/main/java/api/SlackCall.java
+++ src/main/java/api/SlackCall.java
@@ -1,65 +1,25 @@
 package api;
 
-import okhttp3.*;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 
 import java.io.IOException;
 
+
 public class SlackCall {
-    private static final String API_URL = "https://grade-logging-api.chenpan.ca/api/grade";
-    // load API_TOKEN from env variable.
-    private static final String API_TOKEN = System.getenv("API_TOKEN");
-
-    public static String getApiToken() {
-        return API_TOKEN;
-    }
-    public SlackCall() throws JSONException{
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
-        Request request = new Request.Builder()
-                .url("https://slack.com/api/auth.test")
-                .get()
-                .addHeader("Authorization", API_TOKEN)
+    public static void main(String[] args) throws SlackApiException, IOException {
+        Slack slack = Slack.getInstance();
+        final String token = "xoxb-5987663380993-5998028510032-nOhqvPqcDdzpBKzK02PiwuYs";
+        final String channelId = "C05UNTBFKMH";
+        MethodsClient methods = slack.methods(token);
+        ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+                .channel(channelId)
+                .text("Hello there")
                 .build();
-        try {
-            Response response = client.newCall(request).execute();
-            System.out.println(response);
-            JSONObject responseBody = new JSONObject(response.body().string());
-
-        if (responseBody.getBoolean("ok")) {
-            JSONObject url = responseBody.getJSONObject("url");
-            JSONObject team = responseBody.getJSONObject("team");
-            JSONObject user = responseBody.getJSONObject("user");
-            JSONObject team_id = responseBody.getJSONObject("team_id");
-            JSONObject user_id = responseBody.getJSONObject("user_id");
-            JSONObject bot_id = responseBody.getJSONObject("bot_id");
-            JSONObject is_enterprise_install = responseBody.getJSONObject("is_enterprise_install");
-
-        return Team.builder()
-                        .name(team.getString("name"))
-                        .members(members)
-                        .build();
-        }
-        else {
-        throw new RuntimeException(responseBody.getString("message"));
-            }
-        }
-        catch (IOException | JSONException e){
-        throw new RuntimeException(e);
-        }
+        ChatPostMessageResponse response = methods.chatPostMessage(request);
+        System.out.println("The result of the request was a sent message of 'Hello there' to the Slack channel with the channel id C05UNTBFKMH.");
     }
-
-    // example response body
-    //{
-    //    "ok": true,
-    //        "url": "https://7ugroup.slack.com/",
-    //        "team": "7U",
-    //        "user": "7u_encryption",
-    //        "team_id": "T05V1KHB6V7",
-    //        "user_id": "U05VC0UF00Y",
-    //        "bot_id": "B05URE7PN5A",
-    //        "is_enterprise_install": false
-    //}
-
 }
